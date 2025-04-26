074425b514eca8f116b1818eb6322e76ac5d8782
Sivadharshini04 <145845271+Sivadharshini04@users.noreply.github.com>
2025-04-27 02:29:09 +0530

    Create basic chatbot.py

---
+++ basic chatbot.py
@@ -0,0 +1,30 @@
+!pip install pyvirtualdisplay
+!apt-get install -y xvfb
+!python -m pip install tk
+!pip install nltk
+
+import nltk
+from nltk.chat.util import Chat, reflections
+
+nltk.download('punkt')
+nltk.download('wordnet')
+
+#define some patterns and responses
+patterns = [
+    (r'hi|hello|hey', ['Hello!', 'Hi there!', 'Hey!']),
+    (r'how are you?', ['I\'m good, thank you!', 'I\'m doing well, thanks for asking.']),
+    (r'what is your name?', ['You can call me ChatGPT.', 'I go by the name ChatGPT.']),
+    (r'(.*) your name?', ['You can call me ChatGPT.', 'I go by the name ChatGPT.']),
+    (r'can you recommend a book?|show me a book',['Is it ok with Deep Learning', 'shall I go with Gen AI','Can I show ANN']),
+    (r'(.*) Go for a tea?', ['Ya Sure.', 'Not Now, May be Later.', 'With Pleasure'])
+
+]
+
+chatbot = Chat(patterns, reflections)
+
+# start conversation
+print("Hello! I'm ChatGPT. How can I help you today?")
+while True:
+    user_input = input("You: ")
+    response = chatbot.respond(user_input)
+    print("ChatGPT:", response)
